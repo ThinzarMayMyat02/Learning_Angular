473beda907ff68f778281756af6efb7f4e38ca2f
ThinzarMayMyat02 <maymyatthinzar10@gmail.com>
2024-11-27 21:12:39 +0630

    Angular 18 tutorial with Project

---
+++ src/app/components/roles/roles.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-roles',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './roles.component.html',
+  styleUrl: './roles.component.css'
+})
+export class RolesComponent {
+  name : String = "Angular Application";
+  version : number = 18;
+  isActive : boolean = false;
+  currentDate : Date = new Date();
+
+  inputType: string = "radio";
+}
