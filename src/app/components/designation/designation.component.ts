d9e5816d4d53a77b26fc3bb3596bd44cda82439b
May Myat Thinzar <maymyatthinzar@startinpoint.com>
2024-11-28 14:20:29 +0630

    api learning and structural directive

--- src/app/components/designation/designation.component.ts
+++ src/app/components/designation/designation.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MasterService } from '../../services/master.service';
+import { CommonModule } from '@angular/common';
+import { ApiResponseModel, IDesignation } from '../../model/interface/role';
 
 @Component({
   selector: 'app-designation',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './designation.component.html',
   styleUrl: './designation.component.css'
 })
-export class DesignationComponent {
+export class DesignationComponent implements OnInit {
 
+  isLoading: boolean = true;
+  allDesignations:IDesignation [] = [];
+  masterService = inject(MasterService);
+
+  ngOnInit(): void {
+    this.masterService.getDesignations().subscribe((resp:ApiResponseModel)=> {
+                this.allDesignations = resp.data;  
+                this.isLoading = false;     
+    },
+    error=>{
+      this.isLoading = false;
+      alert('Api error/ Network error occured.');
+    }
+  )
+  }
 }
--- src/app/components/roles/roles.component.ts
+++ src/app/components/roles/roles.component.ts
@@ -1,28 +1,45 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ApiResponseModel, IRole } from '../../model/interface/role';
+import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-roles',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css'
 })
-export class RolesComponent {
-  name : string = "Angular Application";
-  version : number = 18;
-  isActive : boolean = false;
-  currentDate : Date = new Date();
+export class RolesComponent implements OnInit{
+  isLoading: boolean = true;
+  http = inject(HttpClient);
+  allRoles: IRole [] = [];
 
-  inputType: string = "radio";
-
-  selectedState: string = '';
-
-  showWelcomeMsg(){
-    alert('Welcome angular tutorial.')
+  ngOnInit(): void {
+    this.getAllRoles();
   }
 
-  showAlertMessage(message: string){
-    alert(message);
+  getAllRoles(){
+   this.http.get('https://freeapi.miniprojectideas.com/api/ClientStrive/GetAllRoles')
+                    .subscribe((resp:any)=>{this.allRoles = resp.data;this.isLoading=false});
   }
+
+  // name : string = "Angular Application";
+  // version : number = 18;
+  // isActive : boolean = false;
+  // currentDate : Date = new Date();
+
+  // inputType: string = "radio";
+
+  // selectedState: string = '';
+
+  // showWelcomeMsg(){
+  //   alert('Welcome angular tutorial.')
+  // }
+
+  // showAlertMessage(message: string){
+  //   alert(message);
+  // }
 }
