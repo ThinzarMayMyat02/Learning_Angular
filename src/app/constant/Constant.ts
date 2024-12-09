ce000d026505ac1f34e88beab00c28cc06c226cb
ThinzarMayMyat02 <maymyatthinzar10@gmail.com>
2024-12-09 23:39:46 +0630

    declarization of Constant

---
+++ src/app/constant/Constant.ts
@@ -0,0 +1,11 @@
+export const Constant = {
+  API_METHOD:{
+    GET_ALL_EMPLOYEE : 'GetAllEmployee',
+    GET_ALL_CLIENT : 'GetAllClients',
+    GET_ALL_CLIENTPROJECT : 'GetAllClientProjects'
+  },
+  VALIDATION_MESSAGE : {
+    REQUIRED : 'This field is required!'
+  }
+
+}
